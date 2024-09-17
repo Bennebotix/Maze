4a8464247c247cb239495f05d28cebf60bb1d81d
Bennett Lang <84034168+Bennebotix@users.noreply.github.com>
2024-09-17 11:24:26 -0500

    Create serviceWorker.js

---
+++ serviceWorker.js
@@ -0,0 +1,48 @@
+const cacheName = 'maze-generator-v2.1'
+const assets = [
+  "/",
+  "/index.html",
+  "/images/icon512_maskable.png",
+  "/images/icon512_rounded.png",
+  "/js/app.js",
+  "manifest.json",
+  "serviceWorker.js"
+];
+
+// Installing the Service Worker
+self.addEventListener("install", async (event) => {
+  try {
+    const cache = await caches.open(cacheName);
+    await cache.addAll(assets);
+  } catch (error) {
+    console.error("Service Worker installation failed:", error);
+  }
+});
+
+// Fetching resources
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    (async () => {
+      const cache = await caches.open(cacheName);
+
+      try {
+        const cachedResponse = await cache.match(event.request);
+        if (cachedResponse) {
+          console.log("cachedResponse: ", event.request.url);
+          return cachedResponse;
+        }
+
+        const fetchResponse = await fetch(event.request);
+        if (fetchResponse) {
+          console.log("fetchResponse: ", event.request.url);
+          await cache.put(event.request, fetchResponse.clone());
+          return fetchResponse;
+        }
+      } catch (error) {
+        console.log("Fetch failed: ", error);
+        const cachedResponse = await cache.match("index.html");
+        return cachedResponse;
+      }
+    })()
+  );
+});
